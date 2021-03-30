aa73513dd766fcf3def6a3985e2587cdeee92cfe
DavDan1 <david-danielyan@hotmail.com>
2021-03-30 12:54:59 +0200

    modified object  and instanceATM

--- specs/atm.spec.js
+++ specs/atm.spec.js
@@ -1,8 +1,19 @@
 const { expecty } = require('chai')
 
-const Atm = require { '../src/atm' }
+const Atm = require('../src/atm')
 
 describe('Atm class', () => {
     let subject
-    before
+
+    before(() =>{
+        subject =new Atm()
+        
+    });
+    it('is expected to be instantiated', () => {
+        expect(subject).to.be.an('object')
+    });
+    it('is expected to be an instance Atm', () => {
+        expect(subject).to.be.instanceOf(Atm)
+    });
+
 });
