832a27390a51706feec13f7e242e5f629f05d3f9
arunbhalli <tomarbhalli.com>
2021-03-30 19:10:16 +0200

    ready to do withdraw amount

--- specs/atm.spec.js
+++ specs/atm.spec.js
@@ -5,15 +5,22 @@ const Atm = require('../src/atm')
 describe('Atm class', () => {
     let subject
 
-    before(() =>{
-        subject =new Atm()
-        
+    before(() => {
+        subject = new Atm()
+
     });
     it('is expected to be instantiated', () => {
         expect(subject).to.be.an('object')
     });
     it('is expected to be an instance Atm', () => {
         expect(subject).to.be.instanceOf(Atm)
+    });
+    it('is expected to hold 1000 currency an initialize', () => {
+        expect(subject.funds).to.equal(1000)
+    });
+    it('is expected to reduce funds on succesful withdrawl', () => {
+        subject.withdraw(150)
+        expect(subject.funds).to.equal(850)
     });
 
 });
